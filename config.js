ff370128645614b3f05c88895f674785d74a7306
minthant <minthantk154@gmail.com>
2023-06-07 23:55:26 +0630

    Signed-off-by: minthant <minthantk154@gmail.com

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Cute Website...💖",
+    introTitle: 'Hello, my crush 💘🌿',
+    introDesc: `You have a beautiful soul that radiates kindness and compassion. 
+    It's one of the things I appreciate most about you."`,
+    btnIntro: '👉Click me👈',
+    title: 'If I were to express a desire to become closer to you, would you choose to allow or reject it? 🥰',
+    desc: "Though I may not mean much to you and we may be different, please don't ignore me.",
+    btnYes: 'Allow ❤',
+    btnNo: 'Reject :(',
+    question: 'In a world of over 8 billion people, I have chosen you and my heart remains hopeful. Despite our current distance, being near you brings me immense joy. Your enchanting smile is a sweet melody that resonates deeply within me. <3',
+    reply: 'It has captured my heart.',
+    btnReply: 'Send',
+    mess: "If you ever find yourself feeling depressed and tired, don't give up and keep trying. I want you to know that I believe in you, and I know you have the strength to overcome any obstacle. Your smile and laughter illuminate your beauty in the most enchanting way. Thank you for reaching this point, and know that I'm here to support you every step of the way.",
+    messDesc: "Could you please share your Facebook account with me? Your kindness would be greatly appreciated, @....",
+    btnAccept: 'Close me :(',
+    messLink: 'https://drive.google.com/file/d/1XxNtD4rTAsARqnctSlLfdVKyoOltSoRZ/view?usp=sharing',
+};
